a452c155b4a7f57ba028d3e4fc80b9af5c221049
Kai Wu <wu.vkcyan@gmail.com>
2020-01-17 15:30:12 +0800

    更新示例demo

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   publicPath: '/',
-  outputDir: 'doc',
+  outputDir: 'docs',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
